7661d367b9b8bad8fa9cd4779a66da5c58064d46
Nadia Lee <miss.nadia.lee@gmail.com>
2017-10-01 03:05:20 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
 	levitation_force = 6
-		while levitation_force == 10
-			puts "Wingardium Leviosa"
+		while levitation_force < 11
 			levitation_force += 1
+			puts "Wingardium Leviosa"
 		end
 end
 using_while
